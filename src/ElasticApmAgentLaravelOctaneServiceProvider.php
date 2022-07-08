127044da7849f9af7b0109f16913feaf891d856d
Niki Ewald Zakariassen <niza@cego.dk>
2022-07-08 14:01:41 +0200

    Initial commit

---
+++ src/ElasticApmAgentLaravelOctaneServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Cego\ElasticApmAgentLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class ElasticApmAgentLaravelOctaneServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+    }
+}
